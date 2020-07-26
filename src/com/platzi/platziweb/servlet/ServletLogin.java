f8318c193d768f1bed39adeea7f0bf42cfbd194e
agdev <agdev@localhost.localdomain>
2020-07-26 21:44:07 +0200

    First commit

---
+++ src/com/platzi/platziweb/servlet/ServletLogin.java
@@ -0,0 +1,57 @@
+package com.platzi.platziweb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+@WebServlet(description = "Servlet para manipular la peticion de login", urlPatterns = { "/ServletLogin" })
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletLogin() {
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("\n");
+		
+		//Procesar la información
+		String usuario = request.getParameter("usuario");
+		String empresa = request.getParameter("empresa");
+		PrintWriter escritor = response.getWriter();
+		
+		if(usuario != null && empresa != null) {
+			if (empresa.equals("Platzi")) {
+				escritor.println("Bienvenido a Platzi.");
+			} else {
+				escritor.println("Bienvenido.");
+			}
+		} else {
+			escritor.println("Usuario incorrecto.");
+		}
+		escritor.close();
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
